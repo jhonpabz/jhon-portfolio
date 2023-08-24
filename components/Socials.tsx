e37c38d012157921a19c76cfad34c19afe00719a
Jhon Cesar Pablo <jhncsrpbl@gmail.com>
2023-08-24 13:16:28 +0800

    added hover effect on socials

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,9 +1,18 @@
 import { Linkedin } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { cn } from "@/lib/utils";
 
 export const Socials = () => {
+  const { theme } = useTheme();
   return (
-    <div className="text-primary/60 mt-6">
-      <Linkedin />
+    <div className="text-primary/60 mt-6 cursor-pointer">
+      <Linkedin
+        className={cn(
+          "transition-colors duration-200",
+          theme === "dark" ? "hover:text-lightGreen" : "hover:text-darkGreen"
+        )}
+      />
     </div>
   );
 };
