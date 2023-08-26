2c7b210760059fc71e9e380917167fc4059ecf03
Jhon Cesar Pablo <jhncsrpbl@gmail.com>
2023-08-26 11:41:20 +0800

    refactor socials

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,49 +1,45 @@
 import { Facebook, Github, Linkedin } from "lucide-react";
 import { useTheme } from "next-themes";
-import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import { BaseNewTabLink } from "./base/BaseNewTabLink";
 
 export const Socials = () => {
   const { theme } = useTheme();
   return (
     <div className="flex gap-4 text-primary/60 mt-6 cursor-pointer">
-      <Link href="https://www.linkedin.com/in/jhoncesarpablo/" legacyBehavior>
-        <a target="_blank" rel="noopener noreferrer" className="mr-3">
-          <Linkedin
-            className={cn(
-              "transition-colors duration-200",
-              theme === "dark"
-                ? "hover:text-lightGreen"
-                : "hover:text-darkGreen"
-            )}
-          />
-        </a>
-      </Link>
-      <Link href="https://github.com/jhonpabz" legacyBehavior>
-        <a target="_blank" rel="noopener noreferrer" className="mr-3">
-          <Github
-            className={cn(
-              "transition-colors duration-200",
-              theme === "dark"
-                ? "hover:text-lightGreen"
-                : "hover:text-darkGreen"
-            )}
-          />
-        </a>
-      </Link>
-      <Link href="https://www.facebook.com/jhoncesarpablo/" legacyBehavior>
-        <a target="_blank" rel="noopener noreferrer" className="mr-3">
-          <Facebook
-            className={cn(
-              "transition-colors duration-200",
-              theme === "dark"
-                ? "hover:text-lightGreen"
-                : "hover:text-darkGreen"
-            )}
-          />
-        </a>
-      </Link>
+      <BaseNewTabLink
+        siteUrl="https://www.linkedin.com/in/jhoncesarpablo/"
+        style="mr-3"
+      >
+        <Linkedin
+          className={cn(
+            "transition-colors duration-200",
+            theme === "dark" ? "hover:text-lightGreen" : "hover:text-darkGreen"
+          )}
+        />
+      </BaseNewTabLink>
+
+      <BaseNewTabLink siteUrl="https://github.com/jhonpabz" style="mr-3">
+        <Github
+          className={cn(
+            "transition-colors duration-200",
+            theme === "dark" ? "hover:text-lightGreen" : "hover:text-darkGreen"
+          )}
+        />
+      </BaseNewTabLink>
+
+      <BaseNewTabLink
+        siteUrl="https://www.facebook.com/jhoncesarpablo/"
+        style="mr-3"
+      >
+        <Facebook
+          className={cn(
+            "transition-colors duration-200",
+            theme === "dark" ? "hover:text-lightGreen" : "hover:text-darkGreen"
+          )}
+        />
+      </BaseNewTabLink>
     </div>
   );
 };
