a87838a7f3477412c4f65b2c6cb67419932d37c0
Jhon Cesar Pablo <jhncsrpbl@gmail.com>
2023-08-26 10:24:00 +0800

    add social links

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,18 +1,37 @@
-import { Linkedin } from "lucide-react";
+import { Github, Linkedin } from "lucide-react";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
 export const Socials = () => {
   const { theme } = useTheme();
   return (
-    <div className="text-primary/60 mt-6 cursor-pointer">
-      <Linkedin
-        className={cn(
-          "transition-colors duration-200",
-          theme === "dark" ? "hover:text-lightGreen" : "hover:text-darkGreen"
-        )}
-      />
+    <div className="flex gap-4 text-primary/60 mt-6 cursor-pointer">
+      <Link href="https://www.linkedin.com/in/jhoncesarpablo/" legacyBehavior>
+        <a target="_blank" rel="noopener noreferrer" className="mr-3">
+          <Linkedin
+            className={cn(
+              "transition-colors duration-200",
+              theme === "dark"
+                ? "hover:text-lightGreen"
+                : "hover:text-darkGreen"
+            )}
+          />
+        </a>
+      </Link>
+      <Link href="https://github.com/jhonpabz" legacyBehavior>
+        <a target="_blank" rel="noopener noreferrer" className="mr-3">
+          <Github
+            className={cn(
+              "transition-colors duration-200",
+              theme === "dark"
+                ? "hover:text-lightGreen"
+                : "hover:text-darkGreen"
+            )}
+          />
+        </a>
+      </Link>
     </div>
   );
 };
