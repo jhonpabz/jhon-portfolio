6c79c692d1bd6d924700a4fb1ded9a6d569cc338
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-08-27 23:31:39 +0800

    revise about

--- components/About.tsx
+++ components/About.tsx
@@ -6,38 +6,38 @@ export const About = () => {
 
       <p className="text-sm text-primary/60 mt-3">
         Back in 2016, during my second year of college as an IT student, there
-        was a subject introduced to us which is Web Design and Client Side
+        was a course introduced to us which is Web Design and Client Side
         Coding. We learn fundamentals which are HTML, CSS, and javascript. I was
-        so hooked and researched more about it on the Internet. There was a time
-        when our professor thought that my project was a template/downloaded to
-        the internet because of how it looked. That was the time my passion for
-        coding starts. Whenever there is a group project my classmates always
-        pick me as a leader/programmer. I create an app using Android Studio and
-        also our Thesis/Capstone project which is created in HTML, CSS,
-        Javascript, PHP, and MySQL.
+        so hooked and researched more about it on the internet. There was a time
+        when our professor thought my project was a template/downloaded to the
+        internet because of how it looked. That was the time my passion for
+        coding started. Whenever there's a group project, my classmates always
+        pick me as a leader/programmer. I created an application using Android
+        Studio, our Thesis/Capstone project using HTML, CSS, Javascript, PHP,
+        and MySQL.
       </p>
 
       <p className="text-sm text-primary/60 mt-3">
         After I graduated in 2019, I applied and got my first job as a Web
-        Designer in DPC. My job there is to create a website for Small and
-        Medium-Scale Enterprises(SMSE) here in the Philippines using HTML, CSS,
-        Javascript, and JQuery. Then one time our manager gave me a special
-        project which is the BAVA vehicle registration system. I finished it and
-        I feel so happy I want to go back in building web application and a more
-        challenging job.
+        Designer at Directories Philippines Corporation. My work there is to
+        create a website for Small and Medium-Scale Enterprises(SMSE) here in
+        the Philippines using HTML, CSS, Javascript, and JQuery. There was a
+        time when my manager gave me a project, the BAVA Vehicle Registration
+        System. I completed it and felt so happy. I want to return to building
+        web applications and have a more challenging job.
       </p>
 
       <p className="text-sm text-primary/60 mt-3">
-        That's why I applied for my second job as a Associate Software Engineer
-        in Fligno Software Philippines Inc. We create web apps for for clients
-        and stakeholders(99% of our clients here is from Australia). All of the
-        project I assigned with, my main role is focuses on my strength which is
-        Front end Developer. We use React, Redux, React Query, Zustand, Material
-        UI, Tailwind and other new technologies for Front-end.
+        That's why I applied for my second job as an Associate Software Engineer
+        at Fligno Software Philippines Inc. We create web applications for
+        clients and stakeholders(99% of our clients here are from Australia). In
+        all of the projects I was assigned, my role was more focused on my
+        strength which was Front-end. We use React, Redux, React Query, Zustand,
+        Material UI, Tailwind, and other new technologies for the Front-end.
       </p>
       <p className="text-sm text-primary/60 mt-3">
-        Today I'm currently working as a Front-end developer in PHILTECH here at
-        Makati. We create web app. Using Nextjs, React, and Nuxtjs/Vuejs.
+        Today, I'm currently working as a Front-end developer in PHILTECH here
+        at Makati. We create web apps using Nextjs, React, and Nuxtjs/Vuejs.
       </p>
     </div>
   );
