107cb9a629f6d83d9151c696d65edae65786c57c
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-10-01 14:59:53 +0800

    update resume

--- components/DownloadPDFButton.tsx
+++ components/DownloadPDFButton.tsx
@@ -5,11 +5,11 @@ import { ArrowRight } from 'lucide-react';
 
 const DownloadPDFButton = () => {
   const handleDownload = () => {
-    const pdfUrl = '/Jhon_Cesar_Pablo-Resume.pdf';
+    const pdfUrl = '/JhonCesarPablo-Resume.pdf';
 
     const anchor = document.createElement('a');
     anchor.href = pdfUrl;
-    anchor.download = 'Jhon_Cesar_Pablo-Resume.pdf';
+    anchor.download = 'JhonCesarPablo-Resume.pdf';
 
     anchor.click();
   };
